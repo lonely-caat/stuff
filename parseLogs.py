1ef5724f4f2e15e9911f286f7535a71901f750a4
lonely-caat <33058683+lonely-caat@users.noreply.github.com>
2020-03-05 18:50:20 +0200

    Update parseLogs.py

--- parseLogs.py
+++ parseLogs.py
@@ -27,9 +27,8 @@ with open(i_f, 'r') as inputfile, open(o_f, 'w') as output_file:
         try:
             name_space,context = split_line[7], split_line[12]
             if name_space == 'm.api.common.server.ApiServerCore' and context == '/api/v3/order':
-                output_line = split_line[0] + ' ' + split_line[1] + '  ' + split_line[14] + ' ' + split_line[
-                    15] + \
-                              ' from ' + split_line[10] + ' ' + split_line[13]
+                output_line = '{0} {1} {2} {3} from {4} {5}'.format(split_line[0], split_line[1], split_line[14],
+                                                                        split_line[15], split_line[10], split_line[13])
                 output_file.write(output_line + '\n')
         except IndexError:
             pass
@@ -48,3 +47,26 @@ with open(i_f, 'r') as inputfile, open(o_f, 'w') as output_file:
 #                 output_file.write(output_line + '\n')
 #         except IndexError:
 #             print(line)
+
+## named tuple lazy loading version
+# from collections import namedtuple
+# logs_list = []
+#
+# with open(i_f, 'r') as inputfile, open(o_f, 'w') as output_file:
+#     for line in read_in_chunks(inputfile):
+#
+#         split_line = line.split()
+#
+#         try:
+#             name_space,context = split_line[7], split_line[12]
+#             if name_space == 'm.api.common.server.ApiServerCore' and context == '/api/v3/order':
+#                 ParsedLogLine = namedtuple('ParsedLogLine', 'date time operation quantity ip currency')
+#                 log = ParsedLogLine(split_line[0], split_line[1], split_line[14],split_line[15],split_line[10], split_line[13])
+#
+#                 output_line = '{0} {1} {2} {3} from {4} {5}'.format(log.date, log.time, log.operation, log.quantity,
+#                                                                     log.ip, log.currency)
+#
+#                 output_file.write(output_line + '\n')
+#                 logs_list.append(output_line)
+#         except IndexError:
+#             pass
