100daaee4d1707900abdb24300fcdc30356adb60
max <33058683+lonely-caat@users.noreply.github.com>
2023-12-28 14:39:00 +0200

    Update youtube.py

--- selenium_scripts/youtube.py
+++ selenium_scripts/youtube.py
@@ -1,69 +1,62 @@
-
-#ScriptName : Login.py
-#---------------------
+import argparse
 from selenium import webdriver
-
-#Following are optional required
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-
-
-baseurl = "https://coub.com/"
-username = ""
-password = ""
-
-xpaths = { "login": '//button[contains(text(), "Log in")]',
-           "first_coub": "//div[@class='viewer__shadow viewer__click -fill']"[0],
-           "controls": "//div[@class='viewer__controls__play']",
-           "facebook": '//div[@page-id="sign-in-page"]//button[contains(text(), "Facebook")]',
-            'email' : "//input[@id='email']",
-            'password' : "//input[@id='pass']",
-           'pause' : "//div[@class='viewer__play viewer__click box--vertical -pause']",
-           'loginButton' : '//input[@name="login"]',
-           'continueButton': '//button[@name="__CONFIRM__"]',
-           'skipButton': '//button[@name="__SKIP__"]',
-
-           'avatar': "//div[@class='box--vertical current header-channel__avatar']",
-
-
-         }
-
-mydriver = webdriver.Chrome()
-mydriver.get(baseurl)
-mydriver.maximize_window()
-
-#silence first coub while we login
-
-ActionChains(mydriver).move_to_element(mydriver.find_element_by_xpath(xpaths['controls'])).perform()
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-WebDriverWait(mydriver, 3).until(EC.presence_of_element_located((By.XPATH, xpaths['pause'])))
-mydriver.find_element_by_xpath(xpaths['pause']).click()
+def scroll_to_end(driver):
+    """Scroll to the end of the page."""
+    last_height = driver.execute_script("return document.documentElement.scrollHeight")
 
-mydriver.find_element_by_xpath(xpaths['login']).click()
-mydriver.find_element_by_xpath(xpaths['facebook']).click()
+    while True:
+        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        try:
+            WebDriverWait(driver, 10).until(
+                lambda d: d.execute_script("return document.documentElement.scrollHeight") > last_height
+            )
+            last_height = driver.execute_script("return document.documentElement.scrollHeight")
+        except TimeoutException:
+            break  # Break the loop if no new content is loaded
 
-# mydriver.find_element_by_xpath(xpaths['email']).click()
-window_before = mydriver.window_handles[0]
-window_after = mydriver.window_handles[1]
+def parse_comments(driver):
+    """Parse comments from the YouTube page."""
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#content #content-text"))
+    )
+    comments = driver.find_elements(By.CSS_SELECTOR, "#content #content-text")
+    return [comment.text for comment in comments]
 
+def main(url):
+    # Setup Chrome options for WebDriver
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Runs Chrome in headless mode.
+    chrome_options.add_argument("--disable-gpu")
 
+    # Initialize WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-#Write Password in password TextBox
-mydriver.switch_to.window(window_after)
-mydriver.find_element_by_xpath(xpaths['email']).send_keys(username)
-mydriver.find_element_by_xpath(xpaths['password']).send_keys(password)
+    try:
+        driver.get(url)
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "ytd-comment-thread-renderer"))
+        )
 
-#Click Login button
-mydriver.find_element_by_xpath(xpaths['loginButton']).click()
-import pdb;pdb.set_trace()
+        # Scroll to load comments
+        scroll_to_end(driver)
 
+        # Parse comments
+        comments = parse_comments(driver)
+        for comment in comments:
+            print(comment)
 
+    finally:
+        driver.quit()
 
-# pause = "//div[@class='viewer__pause__content']"
-# controls = "//div[@class='viewer__controls__play']"
-# mydriver.find_element_by_xpath("//div[@class='viewer__shadow viewer__click -fill']").click()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="YouTube Comment Parser")
+    parser.add_argument("url", help="YouTube video URL to parse comments from")
+    args = parser.parse_args()
+    main(args.url)
